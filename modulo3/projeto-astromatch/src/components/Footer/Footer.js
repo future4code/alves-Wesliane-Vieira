05a2acde0fa0b41dc060d2f35a1c0d115b2adc9b
Wesliane Silva Vieira <weslianebsb2@gmail.com>
2022-07-18 14:40:41 -0300

    add page perfil com funcionalidades dos botões e composição do card

---
+++ modulo3/projeto-astromatch/src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Box, Copyright } from "./FooterStyles";
+
+const Footer = () => {
+return (
+	<Box>
+	
+        	<Copyright>® 2022 Astro Match</Copyright>
+		
+	</Box>
+);
+};
+export default Footer;
+
+
