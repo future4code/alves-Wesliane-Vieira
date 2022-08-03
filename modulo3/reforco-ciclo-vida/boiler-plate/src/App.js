6cc0739f46eb56a243e23c161eafdc675f671478
Wesliane Silva Vieira <weslianebsb2@gmail.com>
2022-08-02 21:19:22 -0300

    ciclo-de-vida local-strorage part1

---
+++ modulo3/reforco-ciclo-vida/boiler-plate/src/App.js
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import "./styles.css";
+
+const TarefaList = styled.ul`
+  padding: 0;
+  width: 200px;
+`;
+
+const Tarefa = styled.li`
+  text-align: left;
+  text-decoration: ${({ completa }) => (completa ? "line-through" : "none")};
+`;
+
+const InputsContainer = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  gap: 10px;
+`;
+
+function App() {
+  const [tarefas, setTarefas] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [filtro, setFiltro] = useState("");
+
+  useEffect(() => {
+    
+  }, []);
+
+  const onChangeInput = (event) => {};
+
+  const criaTarefa = () => {};
+
+  const selectTarefa = (id) => {};
+
+  const onChangeFilter = (event) => {};
+
+  const listaFiltrada = tarefas.filter((tarefa) => {
+    switch (filtro) {
+      case "pendentes":
+        return !tarefa.completa;
+      case "completas":
+        return tarefa.completa;
+      default:
+        return true;
+    }
+  });
+
+  return (
+    <div className="App">
+      <h1>Lista de tarefas</h1>
+      <InputsContainer>
+        <input value={inputValue} onChange={onChangeInput} />
+        <button onClick={criaTarefa}>Adicionar</button>
+      </InputsContainer>
+      <br />
+
+      <InputsContainer>
+        <label>Filtro</label>
+        <select value={filtro} onChange={onChangeFilter}>
+          <option value="">Nenhum</option>
+          <option value="pendentes">Pendentes</option>
+          <option value="completas">Completas</option>
+        </select>
+      </InputsContainer>
+      <TarefaList>
+        {listaFiltrada.map((tarefa) => {
+          return (
+            <Tarefa
+              completa={tarefa.completa}
+              onClick={() => selectTarefa(tarefa.id)}
+            >
+              {tarefa.texto}
+            </Tarefa>
+          );
+        })}
+      </TarefaList>
+    </div>
+  );
+}
+
+export default App;
