68ad97643695b27661f4c43fab6af4d89b66f22e
Wesliane Silva Vieira <weslianebsb2@gmail.com>
2022-08-16 00:10:21 -0300

    tarefa apis e express

---
+++ modulo4/intro-express/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(3003, () => {
+    console.log("Server is running in http://localhost:3003")
+});
+
+app.get("/posts", (req: Request, res: Response) => {
+    res.send("Hello from Express")
+    
+})
