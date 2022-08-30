fdfd4ef556051a8c9abd8a5ad47606b820feec18
Wesliane Silva Vieira <weslianebsb2@gmail.com>
2022-08-29 23:47:22 -0300

    Atividade sobre fluxo no BD

---
+++ modulo4/fluxo-de-dados-backend/src/data.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.produtos = void 0;
+exports.produtos = [
+    {
+        id: 1,
+        name: 'gel de cabelo',
+        price: 40
+    },
+    {
+        id: 2,
+        name: 'sofá',
+        price: 400
+    },
+    {
+        id: 3,
+        name: 'desinfetante',
+        price: 15
+    }
+];
